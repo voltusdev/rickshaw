288673fedb6316b2714c91553bbba6507a2e317f
Jess Scott-Dutcher <jscottdutcher@gmail.com>
2018-12-07 12:56:47 -0500

    Started updating drag-zoom

--- src/js/Rickshaw.Graph.DragZoom.js
+++ src/js/Rickshaw.Graph.DragZoom.js
@@ -36,9 +36,24 @@ Rickshaw.Graph.DragZoom = Rickshaw.Class.create({
 			stopPX: null
 		};
 
-		this.svg.on("mousedown", onMousedown);
+		//this.svg.on("mousedown", onMousedown);
+
+		this.svg.on("pointerdown", onMousedown);
+
+		// function onPointerDown(){
+		// 	var e = d3.event;
+		//
+		// 	if(e.pointerType === "mouse") {
+		// 		var el = d3.select(this);
+		// 		console.log(el);
+		// 		onMousedown(el);
+		// 	}
+		// 	console.log(d3.event);
+		// }
 
 		function onMouseup(datum, index) {
+			var e = d3.event;
+			console.log("up", e);
 			drag.stopDt = pointAsDate(d3.event);
 			var windowAfterDrag = [
 				drag.startDt,
@@ -52,7 +67,6 @@ Rickshaw.Graph.DragZoom = Rickshaw.Class.create({
 			var range = self.graph.window.xMax - self.graph.window.xMin;
 
 			reset(this);
-
 			if (range < self.minimumTimeSelection || isNaN(range)) {
 				return;
 			}
@@ -61,6 +75,8 @@ Rickshaw.Graph.DragZoom = Rickshaw.Class.create({
 		}
 
 		function onMousemove() {
+			var e = d3.event;
+			console.log("move", e);
 			var offset = drag.stopPX = (d3.event.offsetX || d3.event.layerX);
 			if (offset > (self.svgWidth - 1) || offset < 1) {
 				return;
@@ -76,31 +92,62 @@ Rickshaw.Graph.DragZoom = Rickshaw.Class.create({
 			.attr("width", selectionWidth);
 		}
 
-		function onMousedown() {
-			var el = d3.select(this);
-			rectangle = el.append("rect")
-			.style("opacity", self.opacity)
-			.attr("y", 0)
-			.attr("height", "100%");
+		//if long enough mousedown
+		function onPointerDown(){
+			console.log('it was long enough');
+		}
 
-			if(d3.event.preventDefault) {
-				d3.event.preventDefault();
+		function onPointerupTimer(){
+			console.log('lifted touch, too short');
+		}
+
+		function onPointerMoveTimer(){
+			console.log('moved touch, too short');
+		}
+
+		function onMousedown() {
+			var e = d3.event;
+			console.log("down", e);
+			if (e.pointerType === "touch"){
+				var sel = d3.select(this);
+				var timer;
+				//start timer
+				timer = setTimeout(onPointerDown, 500);
+				console.log('timer set');
+				//use these bc they will be overwritten by onMousedown
+				//onup clear the timer
+				d3.select(document).on("pointerup", onPointerupTimer);
+				//on move check the timer, if too short clear timer
+				sel.on("pointermove", onMousemove);
+
+				console.log('touch event');
 			} else {
-				d3.event.returnValue = false;
-			}
-			drag.target = d3.event.target;
-			drag.startDt = pointAsDate(d3.event);
-			drag.startPX = d3.event.offsetX || d3.event.layerX;
-			el.on("mousemove", onMousemove);
-			d3.select(document).on("mouseup", onMouseup);
-			d3.select(document).on("keyup", function() {
-				if (d3.event.keyCode === ESCAPE_KEYCODE) {
-					reset(this);
+				var el = d3.select(this);
+				rectangle = el.append("rect")
+				.style("opacity", self.opacity)
+				.attr("y", 0)
+				.attr("height", "100%");
+
+				if(d3.event.preventDefault) {
+					d3.event.preventDefault();
+				} else {
+					d3.event.returnValue = false;
 				}
-			});
+				drag.target = d3.event.target;
+				drag.startDt = pointAsDate(d3.event);
+				drag.startPX = d3.event.offsetX || d3.event.layerX;
+				el.on("pointermove", onMousemove);
+				d3.select(document).on("pointerup", onMouseup);
+				d3.select(document).on("keyup", function() {
+					if (d3.event.keyCode === ESCAPE_KEYCODE) {
+						reset(this);
+					}
+				});
+			}
 		}
 
 		function reset(el) {
+			console.log('reset');
 			var s = d3.select(el);
 			s.on("mousemove", null);
 			d3.select(document).on("mouseup", null);
