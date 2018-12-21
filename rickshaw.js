0dd9ab9cc9d6872c11a4abe213f4537e6d93ebb4
maxmcd <max.t.mcdonnell@gmail.com>
2018-12-21 13:24:26 -0500

    1.6.10

--- rickshaw.js
+++ rickshaw.js
@@ -12,7 +12,7 @@
 /* jshint -W079 */
 
 var Rickshaw = {
-  version: '1.6.9',
+  version: '1.6.10',
 
   namespace: function(namespace, obj) {
     var parts = namespace.split('.')
--- src/js/Rickshaw.js
+++ src/js/Rickshaw.js
@@ -1,7 +1,7 @@
 /* jshint -W079 */
 
 var Rickshaw = {
-  version: '1.6.9',
+  version: '1.6.10',
 
   namespace: function(namespace, obj) {
     var parts = namespace.split('.')
